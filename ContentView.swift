6c4a07763a1e05411b8ace9dedad6408002ad987
Patryk Mocczydlowski <pmoczydlowski5523@stu.d214.org>
2023-11-28 10:46:34 -0600

    commit

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,53 @@
 import SwiftUI
 
+struct Standing: Codable {
+    // Define the structure to represent standing data
+    let league_name: String
+    let team_name: String
+    let overall_league_position: String
+}
+
 struct ContentView: View {
+    @State private var standings: [Standing] = []
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(standings, id: \.team_name) { standing in
+                VStack(alignment: .leading) {
+                    Text("\(standing.team_name)")
+                        .font(.headline)
+                    Text("Position: \(standing.overall_league_position)")
+                        .font(.subheadline)
+                }
+            }
+            .navigationTitle("Soccer Standings")
+            .onAppear(perform: loadData)
         }
+    }
+
+    func loadData() {
+        guard let url = URL(string: "https://api-football-standings.azharimm.site/leagues") else {
+            print("Invalid URL")
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                do {
+                    let standings = try JSONDecoder().decode([Standing].self, from: data)
+                    DispatchQueue.main.async {
+                        self.standings = standings
+                    }
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }.resume()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }

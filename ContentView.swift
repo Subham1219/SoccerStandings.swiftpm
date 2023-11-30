abe1d471c6d63a3b96b6e74bee52eb3d6cacc054
Patryk Mocczydlowski <pmoczydlowski5523@stu.d214.org>
2023-11-30 09:59:18 -0600

    commit

--- ContentView.swift
+++ ContentView.swift
@@ -1,53 +1,19 @@
 import SwiftUI
 
-struct Standing: Codable {
-    // Define the structure to represent standing data
-    let league_name: String
-    let team_name: String
-    let overall_league_position: String
-}
-
 struct ContentView: View {
-    @State private var standings: [Standing] = []
-
     var body: some View {
-        NavigationView {
-            List(standings, id: \.team_name) { standing in
-                VStack(alignment: .leading) {
-                    Text("\(standing.team_name)")
-                        .font(.headline)
-                    Text("Position: \(standing.overall_league_position)")
-                        .font(.subheadline)
-                }
+        VStack {
+            Text("Soccer Leagues Standings")
+                .font(.largeTitle)
+            Image(systemName: "figure.soccer")
+                .font(.largeTitle)
+        
+                .padding()
+                .padding()
+            
+            NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/
             }
-            .navigationTitle("Soccer Standings")
-            .onAppear(perform: loadData)
         }
-    }
-
-    func loadData() {
-        guard let url = URL(string: "https://api-football-standings.azharimm.site/leagues") else {
-            print("Invalid URL")
-            return
-        }
-
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let data = data {
-                do {
-                    let standings = try JSONDecoder().decode([Standing].self, from: data)
-                    DispatchQueue.main.async {
-                        self.standings = standings
-                    }
-                } catch {
-                    print(error.localizedDescription)
-                }
-            }
-        }.resume()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
---
+++ premierLeague.swift
@@ -0,0 +1,20 @@
+//
+//  premierLeague.swift
+//  SoccerStandings
+//
+//  Created by Patryk Moczydlowski on 11/28/23.
+//
+
+import SwiftUI
+
+struct premierLeague: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct premierLeague_Previews: PreviewProvider {
+    static var previews: some View {
+        premierLeague()
+    }
+}

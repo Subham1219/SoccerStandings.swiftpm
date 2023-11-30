72e2dc45d651ba1e0491618dc54e7d9dbe6c482f
Patryk Mocczydlowski <pmoczydlowski5523@stu.d214.org>
2023-11-30 10:48:41 -0600

    commit

--- ContentView.swift
+++ ContentView.swift
@@ -17,6 +17,10 @@ struct ContentView: View {
                     
                         .padding()
                         .padding()
+                        .padding()
+                        .padding()
+                        .padding()
+                    
                     
                     NavigationLink("Premier League") {
                         premierLeague()
